a57ce126114d04302a652d82d4a9bd1a12c2fe8a
SergioZ <serzupo@gmail.com>
2020-09-22 20:00:52 +0200

    Ejercicio3

---
+++ Ejercicio3/js/funciones.js
@@ -0,0 +1,21 @@
+function mostrarVariables() {
+
+    alert('Variable1: ' + variable1);
+
+
+    Variable2 = null;
+    alert(Variable2);
+
+    var variable1 = 34;
+
+    alert(variable1);
+
+    variable3 = 15, 9;
+    alert((variable1 * variable3));
+
+    variable3 = "Ahora soy un texto";
+
+    alert(variable3);
+
+    alert((variable3 * 35));
+}
