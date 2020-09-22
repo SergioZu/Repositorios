80deaca02b3224ffe003257170f5df3fc49291ad
SergioZ <serzupo@gmail.com>
2020-09-22 19:30:23 +0200

    Ejercicio2

---
+++ Ejercicio2/js/javascript.js
@@ -0,0 +1,12 @@
+let var1;
+
+var1 = "Soy la variable 1";
+console.log(var1);
+var1 = ["5", "45", "rojo"];
+console.log(var1);
+var1 = 56.7;
+console.log(var1);
+var1 = null;
+console.log(var1);
+var1 = undefined;
+console.log(var1);
