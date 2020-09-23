53c63f0b01e241ca95c29b5ec746fa921a778cc4
SergioZ <serzupo@gmail.com>
2020-09-23 20:25:08 +0200

    Ejercicio6

--- Ejercicio6/js/funciones.js
+++ Ejercicio6/js/funciones.js
@@ -17,3 +17,28 @@ function factorial() {
     }
     alert(resultado);
 }
+
+function edad() {
+    var edad;
+    edad = prompt("Dime tu edad: ");
+
+    if (edad < 7) {
+        alert("Patrulla Canina");
+    } else if (edad > 7 && edad < 13) {
+        alert("Alexa y Katie");
+    } else if (edad > 14 && edad < 18) {
+        alert("Anne with an E");
+    } else if (edad > 19 && edad < 35) {
+        alert("Stranger Things");
+    } else if (edad > 35) {
+        alert("Sherlock ");
+    }
+}
+
+function bucle() {
+    var valor;
+    do {
+        valor = prompt("Dime un numero: ");
+
+    } while (valor != 45 && valor != 72);
+}
