f1e434c3504c8ba066fb7375321b726eb0329015
SergioZ <serzupo@gmail.com>
2020-09-23 18:47:37 +0200

    Ejercicio4

---
+++ Ejercicio4/js/funciones.js
@@ -0,0 +1,25 @@
+function comprobarOperadores() {
+    var1 = "hola";
+    var2 = "adios";
+
+    alert(var1 == var2);
+    alert(var1 != var2);
+    alert(var1 < var2);
+    alert(var1 > var2);
+    alert(var1 <= var2);
+    alert(var1 >= var2);
+
+    var3 = 4;
+    var4 = "cuatro";
+    alert(var3 == var4);
+    alert(var3 === var4);
+
+    var5 = 4;
+    var6 = 7;
+    var7 = 10;
+    alert(var5 < var6);
+    alert(var6 < var7);
+    alert(var5 <= var6);
+    alert(var6 > var7);
+    alert(var7 != true);
+}
